06c3a3cb13f5b95c1a059c2cada6eb27772a2e1b
faultylegend <48661622+faultylegend@users.noreply.github.com>
2021-04-10 14:21:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,7 @@ def main():
 
     end = time.perf_counter()
 
-    for i in anime_list:
-        print(i[0],get_links(i[0]))
+    # print(anime_list[0][0],get_links(anime_list[0][0]))
 
     print(f'finished in {round(end-start,2)} second(s)')
 
