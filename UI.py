dc587ea60a42a5267557a3f3f7c779664ac69a1f
hscottvo <hscottvo@gmail.com>
2021-04-11 03:38:46 -0700

    Final python commits
    - Python is hard
    - It works most of the time
    - We did more than previous hackathons :^)

--- UI.py
+++ UI.py
@@ -2,16 +2,20 @@ from tkinter import *
 from tkinter.ttk import Combobox, Separator
 from tkinter import messagebox
 import time
+import pandas as pd
 from classes import *
 from video import *
 from justwatch_query import * 
 from anime_comfy_levels import genre_finder, anime_levels
 #Creating tkinter window
-intensitylevel = 1
+
+df = pd.read_csv("saved.csv")
+# df = df.sort_values(by='Ratio')
+
+
+
 def anime_click(title):
     open_link(get_links(title))
-def print_intensity():
-    print(intensitylevel)
 class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -23,134 +27,140 @@ class Window(Frame):
 
         
     def create_rootwindow(self):
-        self.master.geometry('650x270')
+        self.master.geometry('1200x600')
         self.master.title('MALdoro')
         
-        self.create_upperframe()
+        # self.create_upperframe()
         self.create_lowerframe()
-        self.create_upperentries()
+        # self.create_upperentries()
         self.create_lowerentries()
         #minute, second = self.create_lowerentries()
         #self.update_clock()
         #self.submit(minute, second)
-        self.upperframe.pack(side="right", fill="both", expand=False, padx=4)
+        # self.upperframe.pack(side="right", fill="both", expand=False, padx=4)
         self.lowerframe.pack(side="left", fill="both", expand=True, padx=4, pady=4)
         
         
         #self.countdown()
         ###############################################################################try to add a pause button
-    def create_upperframe(self):
-        self.upperframe = Frame(self.master, width=980, height=490)
-        self.upperframe.config(background="#ffffff")
-        # Vseparator = Separator(self.upperframe, orient='vertical')
-        # Vseparator.grid(column = 1, row = 2, columnspan = 2 , sticky = E)
+    # def create_upperframe(self):
+    #     self.upperframe = Frame(self.master, width=980, height=490)
+    #     self.upperframe.config(background="#ffffff")
+    #     # Vseparator = Separator(self.upperframe, orient='vertical')
+    #     # Vseparator.grid(column = 1, row = 2, columnspan = 2 , sticky = E)
 
     def create_lowerframe(self):
         self.lowerframe = Frame(self.master, width=830, height=200)
         self.lowerframe.config(background="#a3a4ad")
-    def create_upperentries(self):
-        self.create_upperframe()
-        GenreList=["H","Harem","Action","Adventure","Mystery","Romance","Fantasy","Psychological","School","Shounen","Magic","Slice of Life",]
-        length = len(GenreList)
-        half = length // 2
-        c = 6
-        for i in range(0, half):
-            r = i +10
-            
-            Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r, column=c+5, pady=4, padx=0, sticky = W)
-            self.upperframe.grid_columnconfigure(c+1, weight=1)
-        for i in range(half, length):
-            r2 = i - half + 10 
-            
-            Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r2, column=c+6, pady=4, padx=0, sticky = W)
-            self.upperframe.grid_columnconfigure(c+1, weight=1)
 
-        r = 2
-        ################## ToDo Drop Down
-        Label(self.upperframe, text="Intensity level : ",font = ("Times New Roman", 10)).grid(row=r, column=c+5, pady=4, padx=0, sticky = S)
-        self.upperframe.grid_columnconfigure(c+1, weight=1)
-        n = StringVar()
-        IntensityLvl = Combobox(self.upperframe, width = 27, textvariable = n, state= "readonly")
-        IntensityLvl['values'] = ('1', '2','3','4','5','6','7','8','9','10')
-        IntensityLvl.grid(column = c+6, row = r)
-        intensitylevel = IntensityLvl.get()
-        self.upperframe.grid_columnconfigure(c+1, weight=1)        
-        ################## submit button        
-        Submit = Button(self.upperframe, text="Submit", command = print_intensity)
-        Submit.grid(column = c+6, row = r+20, sticky = E)
+    # def create_upperentries(self):
+    #     self.create_upperframe()
+    #     GenreList=["Comedy","Horror","Action","Adventure","Mystery","Romance","Fantasy","Psychological","School","Shounen","Magic","Slice of Life",]
+    #     length = len(GenreList)
+    #     half = length // 2
+    #     c = 6
+    #     for i in range(0, half):
+    #         r = i +10
+            
+    #         Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r, column=c+5, pady=4, padx=0, sticky = W)
+    #         self.upperframe.grid_columnconfigure(c+1, weight=1)
+    #     for i in range(half, length):
+    #         r2 = i - half + 10 
+            
+    #         Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r2, column=c+6, pady=4, padx=0, sticky = W)
+    #         self.upperframe.grid_columnconfigure(c+1, weight=1)
+
+    #     r = 2
+    #     ################## ToDo Drop Down
+    #     Label(self.upperframe, text="Intensity level : ",font = ("Times New Roman", 10)).grid(row=r, column=c+5, pady=4, padx=0, sticky = S)
+    #     self.upperframe.grid_columnconfigure(c+1, weight=1)
+    #     n = StringVar()
+    #     IntensityLvl = Combobox(self.upperframe, width = 27, textvariable = n, state= "readonly")
+    #     IntensityLvl['values'] = ('1', '2','3','4','5','6','7','8','9','10')
+    #     IntensityLvl.grid(column = c+6, row = r)
+    #     intensitylevel = IntensityLvl.get()
+    #     self.upperframe.grid_columnconfigure(c+1, weight=1)        
+    #     ################## submit button        
+    #     Submit = Button(self.upperframe, text="Submit", command = print_intensity)
+    #     Submit.grid(column = c+6, row = r+20, sticky = E)
     
     def create_lowerentries(self):
         self.create_lowerframe()
-        AnimeList=["Demon Slayer","Nisekoi","Jujutsu Kaisen","Adventure","Mystery","Romance","Fantasy","Psychological","School","Shounen","Magic","Slice of Life",]
+        # AnimeList=df.loc[0:10]
+        AnimeList=df.loc[0:10, ~df.columns.str.match("Unnamed")]
         length = len(AnimeList)
-        half = length // 2
         c = 0
     # Anime Titles Column    
         Label(self.lowerframe, text="Anime Titles").grid(column=c+1, pady=4, padx=0, sticky = W)
-        
-        for i in range(0, half):
+        # print(AnimeList[0]['Title'])
+        for i in range(0, length):
             r = i +10
             
-            Button(self.lowerframe, text=AnimeList[i], command=lambda title=AnimeList[i]: anime_click(title)).grid(row=r, column=c+1, pady=4, padx=0, sticky = W)
+            Button(self.lowerframe, text=AnimeList.loc[i]['Title'], command=lambda *args: anime_click(AnimeList['Title'][i])).grid(row=r, column=c+1, pady=4, padx=0, sticky = W)
             self.lowerframe.grid_columnconfigure(c+2, weight=1)
     # Anime Intensity
-        Label(self.lowerframe, text="Anime Intensity Level").grid(row = 0, column=c+2, pady=4, padx=0, sticky = W)
-        for i in range(0, half):
+        Label(self.lowerframe, text="Genres").grid(row = 0, column=c+2, pady=4, padx=0, sticky = W)
+        for i in range(0, length):
             r2= i +10
             
-            Label(self.lowerframe, text=AnimeList[i]).grid(row=r2, column=c+2, pady=4, padx=0, sticky = W)
+            Label(self.lowerframe, text=AnimeList.loc[i]['Genres']).grid(row=r2, column=c+2, pady=4, padx=0, sticky = W)
             self.lowerframe.grid_columnconfigure(c+1, weight=1)
-        Label(self.lowerframe, text="Genres").grid(row = 0, column=c+3, pady=4, padx=2, sticky = W)
-        for i in range(0, half):
+        Label(self.lowerframe, text="Anime Intensity Level").grid(row = 0, column=c+3, pady=4, padx=2, sticky = W)
+        for i in range(0, length):
             r= i +10
             
-            Label(self.lowerframe, text=AnimeList[i]).grid(row=r, column=c+3, pady=4, padx=0, sticky = W)
+            Label(self.lowerframe, text=AnimeList.loc[i]["Ratio"]).grid(row=r, column=c+3, pady=4, padx=0, sticky = W)
             self.lowerframe.grid_columnconfigure(c+1, weight=1)
-    def submit(self, minute, second):
-            r=2
-            c=2
-            self.create_lowerframe()
-            #temp = int(minute.get()) * 60 + int(second.get())
-            try:
-                # the input provided by the user is
-                # stored in here :temp
-                temp =  int(minute.get())*60 + int(second.get())
-            except:
-                print("Please input the right value")
-            while temp >-1:
+    # def submit(self, minute, second):
+    #         r=2
+    #         c=2
+    #         self.create_lowerframe()
+    #         #temp = int(minute.get()) * 60 + int(second.get())
+    #         try:
+    #             # the input provided by the user is
+    #             # stored in here :temp
+    #             temp =  int(minute.get())*60 + int(second.get())
+    #         except:
+    #             print("Please input the right value")
+    #         while temp >-1:
                 
-                # divmod(firstvalue = temp//60, secondvalue = temp%60)
-                mins,secs = divmod(temp,60)
+    #             # divmod(firstvalue = temp//60, secondvalue = temp%60)
+    #             mins,secs = divmod(temp,60)
         
-                # Converting the input entered in mins or secs to
-                # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
-                # 50min: 0sec)
+    #             # Converting the input entered in mins or secs to
+    #             # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+    #             # 50min: 0sec)
                
-                # using format () method to store the value up to
-                # two decimal places
+    #             # using format () method to store the value up to
+    #             # two decimal places
                 
-                minute.set("{0:2d}".format(mins))
-                second.set("{0:2d}".format(secs))
+    #             minute.set("{0:2d}".format(mins))
+    #             second.set("{0:2d}".format(secs))
         
-                # updating the GUI window after decrementing the
-                # temp value every time
-                self.update()
-                time.sleep(1)
+    #             # updating the GUI window after decrementing the
+    #             # temp value every time
+    #             self.update()
+    #             time.sleep(1)
         
-                # when temp value = 0; then a messagebox pop's up
-                # with a message:"Time's up"
-                if (temp == 0):
-                    messagebox.showinfo("Time Countdown", "Time's up ")
+    #             # when temp value = 0; then a messagebox pop's up
+    #             # with a message:"Time's up"
+    #             if (temp == 0):
+    #                 messagebox.showinfo("Time Countdown", "Time's up ")
                 
-                # after every one sec the value of temp will be decremented
-                # by one
-                temp -= 1       
+    #             # after every one sec the value of temp will be decremented
+    #             # by one
+    #             temp -= 1       
 
     
-def main():
+def ui_main():
     window = Tk()
     app = Window(window)
     window.mainloop()
     
     
-# main()
+
+if __name__ == "__main__":
+    ui_main()
+    # print(df.loc[0:10])
+    # AnimeList=df.loc[0:10, ~df.columns.str.match("Unnamed")]
+    # print(AnimeList.loc[0]['Ratio'])
--- main.py
+++ main.py
@@ -9,12 +9,175 @@ import concurrent.futures
 import threading
 import multiprocessing
 import time
-import UI
+from UI import Window
+from tkinter import *
+from tkinter.ttk import Combobox, Separator
+from tkinter import messagebox
 
 genres_list = Genres()
 anime_id_new = []
 anime_id_completed = []
 
+from tkinter import *
+from tkinter.ttk import Combobox, Separator
+from tkinter import messagebox
+import time
+import pandas as pd
+from classes import *
+from video import *
+from justwatch_query import * 
+from anime_comfy_levels import genre_finder, anime_levels
+#Creating tkinter window
+
+df = pd.read_csv("saved.csv")
+# df = df.sort_values(by='Ratio')
+final_list = []
+
+
+
+def anime_click(title):
+    open_link(get_links(title))
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        # create master window
+        self.master = master
+        
+        # create root window on initialization
+        self.create_rootwindow()
+
+        
+    def create_rootwindow(self):
+        self.master.geometry('1200x600')
+        self.master.title('MALdoro')
+        
+        # self.create_upperframe()
+        self.create_lowerframe()
+        # self.create_upperentries()
+        self.create_lowerentries()
+        #minute, second = self.create_lowerentries()
+        #self.update_clock()
+        #self.submit(minute, second)
+        # self.upperframe.pack(side="right", fill="both", expand=False, padx=4)
+        self.lowerframe.pack(side="left", fill="both", expand=True, padx=4, pady=4)
+        
+        
+        #self.countdown()
+        ###############################################################################try to add a pause button
+    # def create_upperframe(self):
+    #     self.upperframe = Frame(self.master, width=980, height=490)
+    #     self.upperframe.config(background="#ffffff")
+    #     # Vseparator = Separator(self.upperframe, orient='vertical')
+    #     # Vseparator.grid(column = 1, row = 2, columnspan = 2 , sticky = E)
+
+    def create_lowerframe(self):
+        self.lowerframe = Frame(self.master, width=830, height=200)
+        self.lowerframe.config(background="#a3a4ad")
+
+    # def create_upperentries(self):
+    #     self.create_upperframe()
+    #     GenreList=["Comedy","Horror","Action","Adventure","Mystery","Romance","Fantasy","Psychological","School","Shounen","Magic","Slice of Life",]
+    #     length = len(GenreList)
+    #     half = length // 2
+    #     c = 6
+    #     for i in range(0, half):
+    #         r = i +10
+            
+    #         Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r, column=c+5, pady=4, padx=0, sticky = W)
+    #         self.upperframe.grid_columnconfigure(c+1, weight=1)
+    #     for i in range(half, length):
+    #         r2 = i - half + 10 
+            
+    #         Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r2, column=c+6, pady=4, padx=0, sticky = W)
+    #         self.upperframe.grid_columnconfigure(c+1, weight=1)
+
+    #     r = 2
+    #     ################## ToDo Drop Down
+    #     Label(self.upperframe, text="Intensity level : ",font = ("Times New Roman", 10)).grid(row=r, column=c+5, pady=4, padx=0, sticky = S)
+    #     self.upperframe.grid_columnconfigure(c+1, weight=1)
+    #     n = StringVar()
+    #     IntensityLvl = Combobox(self.upperframe, width = 27, textvariable = n, state= "readonly")
+    #     IntensityLvl['values'] = ('1', '2','3','4','5','6','7','8','9','10')
+    #     IntensityLvl.grid(column = c+6, row = r)
+    #     intensitylevel = IntensityLvl.get()
+    #     self.upperframe.grid_columnconfigure(c+1, weight=1)        
+    #     ################## submit button        
+    #     Submit = Button(self.upperframe, text="Submit", command = print_intensity)
+    #     Submit.grid(column = c+6, row = r+20, sticky = E)
+    
+    def create_lowerentries(self):
+        self.create_lowerframe()
+        # AnimeList=df.loc[0:10]
+        
+        length = len(final_list)
+        c = 0
+    # Anime Titles Column    
+        Label(self.lowerframe, text="Anime Titles").grid(column=c+1, pady=4, padx=0, sticky = W)
+        # print(AnimeList[0]['Title'])
+        for i in range(0, length):
+            r = i +10
+            Button(self.lowerframe, text=final_list[i].title, command=lambda title=final_list[i].title: anime_click(title)).grid(row=r, column=c+1, pady=4, padx=0, sticky = W)
+            self.lowerframe.grid_columnconfigure(c+2, weight=1)
+    # Anime Intensity
+        Label(self.lowerframe, text="Genres").grid(row = 0, column=c+2, pady=4, padx=0, sticky = W)
+        for i in range(0, length):
+            r2= i +10
+            
+            Label(self.lowerframe, text=final_list[i].genres).grid(row=r2, column=c+2, pady=4, padx=0, sticky = W)
+            self.lowerframe.grid_columnconfigure(c+1, weight=1)
+        Label(self.lowerframe, text="Anime Intensity Level").grid(row = 0, column=c+3, pady=4, padx=2, sticky = W)
+        for i in range(0, length):
+            r= i +10
+            
+            Label(self.lowerframe, text=final_list[i].ratio()).grid(row=r, column=c+3, pady=4, padx=0, sticky = W)
+            self.lowerframe.grid_columnconfigure(c+1, weight=1)
+    # def submit(self, minute, second):
+    #         r=2
+    #         c=2
+    #         self.create_lowerframe()
+    #         #temp = int(minute.get()) * 60 + int(second.get())
+    #         try:
+    #             # the input provided by the user is
+    #             # stored in here :temp
+    #             temp =  int(minute.get())*60 + int(second.get())
+    #         except:
+    #             print("Please input the right value")
+    #         while temp >-1:
+                
+    #             # divmod(firstvalue = temp//60, secondvalue = temp%60)
+    #             mins,secs = divmod(temp,60)
+        
+    #             # Converting the input entered in mins or secs to
+    #             # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+    #             # 50min: 0sec)
+               
+    #             # using format () method to store the value up to
+    #             # two decimal places
+                
+    #             minute.set("{0:2d}".format(mins))
+    #             second.set("{0:2d}".format(secs))
+        
+    #             # updating the GUI window after decrementing the
+    #             # temp value every time
+    #             self.update()
+    #             time.sleep(1)
+        
+    #             # when temp value = 0; then a messagebox pop's up
+    #             # with a message:"Time's up"
+    #             if (temp == 0):
+    #                 messagebox.showinfo("Time Countdown", "Time's up ")
+                
+    #             # after every one sec the value of temp will be decremented
+    #             # by one
+    #             temp -= 1       
+
+    
+def ui_main():
+    window = Tk()
+    app = Window(window)
+    window.mainloop()
+    
+    
 
 
 # self.title = title
@@ -141,6 +304,7 @@ def main():
     intensity_level = 10 - user_level
     lower_bound = intensity_level - 1
     upper_bound = intensity_level + 1
+    global final_list
     final_list = []
     for i in genre_specific:
         show_level = i.ratio()
@@ -152,10 +316,9 @@ def main():
     else:
         for show in final_list:
             print(show.title,show.ratio())
-    
     # print(genre_specific[0].title, get_links(genre_specific[0].title))
     # save_list(anime_list)
-
+    ui_main()
 
 if __name__ == "__main__":
     main()
