109a79cb5922919096539eedf9c1c61c49e4b485
Badminininja <57570206+Badminininja@users.noreply.github.com>
2021-04-10 20:36:24 -0700

    formatted to the dynamic size
    formatted to the dynamic size such that changing the dimensions of the window will change where the buttons and more are. creating the start of timer function

--- UI.py
+++ UI.py
@@ -29,117 +29,124 @@
 
 # window.mainloop()
 
-########################################
-# import tkinter as tk
-# from tkinter import ttk
-
-# # Creating tkinter window
-# window = tk.Tk()
-# window.geometry('350x250')
-# # Label
-# ttk.Label(window, text = "To Do level : ", 
-#         font = ("Times New Roman", 10)).grid(column = 0, 
-#         row = 15, padx = 10, pady = 25)
-  
-# n = tk.StringVar()
-# ToDoLvl = ttk.Combobox(window, width = 27, 
-#                             textvariable = n, state= "readonly")
-  
-# # Adding combobox drop down list
-# ToDoLvl['values'] = ('1', 
-#                      '2',
-#                      '3',
-#                      '4',
-#                      '5',)
-  
-# ToDoLvl.grid(column = 1, row = 15)
-
-
-# # Shows february as a default value
-# ToDoLvl.current(1) 
-# window.mainloop()
-
-
-
-
 
 from tkinter import *
 from tkinter.ttk import Combobox, Separator
+import time
 #Creating tkinter window
+
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        # create master window
+        self.master = master
+
+        # create root window on initialization
+        self.create_rootwindow()
+
+    def create_rootwindow(self):
+        self.master.geometry('750x350')
+        self.master.title('MALdoro')
+        
+        self.create_upperframe()
+        self.create_lowerframe()
+        self.create_buttonentries()
+        self.update_clock()
+        self.upperframe.pack(side="top", fill="both", expand=True, padx=4)
+        self.lowerframe.pack(side="left", fill="both", expand=True, padx=4, pady=4)
+        
+        
+        #self.countdown()
+        ###############################################################################try to add a pause button
+    def create_upperframe(self):
+        self.upperframe = Frame(self.master, width=980, height=490)
+        self.upperframe.config(background="#ffffff")
+        # Vseparator = Separator(self.upperframe, orient='vertical')
+        # Vseparator.grid(column = 1, row = 2, columnspan = 2 , sticky = E)
+
+    def create_lowerframe(self):
+        self.lowerframe = Frame(self.master, width=830, height=200)
+        self.lowerframe.config(background="#a3a4ad")
+    def create_buttonentries(self):
+        self.create_upperframe()
+        GenreList=["Ecchi","Harem","Action","Adventure","Mystery","Romance","Fantasy","Psychological","School","Shounen","Magic","Slice of Life",]
+        length = len(GenreList)
+        half = length // 2
+        c = 6
+        for i in range(0, half):
+            r = i +10
+            
+            Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r, column=c+5, pady=4, padx=0, sticky = W)
+            self.upperframe.grid_columnconfigure(c+1, weight=1)
+        for i in range(half, length):
+            r2 = i - half + 10 
+            
+            Checkbutton(self.upperframe, text=GenreList[i]).grid(row=r2, column=c+6, pady=4, padx=0, sticky = W)
+            self.upperframe.grid_columnconfigure(c+1, weight=1)
+
+        r = 2
+        ################## ToDo Drop Down
+        Label(self.upperframe, text="To Do level : ",font = ("Times New Roman", 10)).grid(row=r, column=c+5, pady=4, padx=0, sticky = S)
+        self.upperframe.grid_columnconfigure(c+1, weight=1)
+        n = StringVar()
+        ToDoLvl = Combobox(self.upperframe, width = 27, textvariable = n, state= "readonly")
+        ToDoLvl['values'] = ('1', '2','3','4','5','6','7','8','9','10')
+        ToDoLvl.grid(column = c+6, row = r)
+        ToDoLvl.current(1)
+        self.upperframe.grid_columnconfigure(c+1, weight=1)        
+        ################## submit button        
+        Submit = Button(self.upperframe, text="Submit")
+        Submit.grid(column = c+6, row = r+20, sticky = E)
+    def update_clock(self):
+        c=1
+        r=2
+        self.create_lowerframe()
+        now = time.strftime("%H:%M:%S")
+
+        Label(self.lowerframe,text = now).grid(row=r, column=c+5, pady=4, padx=0)
+        self.lowerframe.grid_rowconfigure(2)
+        self.master.after(10, self.update_clock)
+    # def countdown(self):
+    #     self.create_lowerframe()
+    #     t=10 #1500 is 25 minutes
+    #     r = 2
+    #     c = 6
+    #     while t:
+    #         mins, secs = divmod(t, 60)
+            
+    #         timer = '{:02d}:{:02d}'.format(mins, secs)
+    #         Label(self.lowerframe, text=timer,font = ("Times New Roman", 10)).grid(row=r, column=c+5, pady=4, padx=0, sticky = S)
+            
+    #         time.sleep(1)
+    #         t -= 1
+    #     print('Fire in the hole!!')
+
+
 window = Tk()
-window.geometry('650x750')
-window.title('MALdoro')
-
-####################  Buttons
-Submit = Button(window, text="Submit")
-Submit.place(x=550, y= 450)
-
-####################  Labels
-lbl=Label(window, text="Genres", fg='black', font=("Times New Roman", 12))
-lbl.place(x=450, y=100)
-
-
-####################  Bunch of Checkboxes 
-#########Genre Checkboxes
-v1 = IntVar()
-v2 = IntVar()
-v3 = IntVar()
-v4 = IntVar()
-v5 = IntVar()
-v6 = IntVar()
-v7 = IntVar()
-v8 = IntVar()
-v9 = IntVar()
-v10 = IntVar()
-v11 = IntVar()
-v12 = IntVar()
-C1 = Checkbutton(window, text = "Ecchi", variable = v1)
-C2 = Checkbutton(window, text = "Harem", variable = v2)
-C3 = Checkbutton(window, text = "Action", variable = v3)
-C4 = Checkbutton(window, text = "Adventure", variable = v4)
-C5 = Checkbutton(window, text = "Mystery", variable = v5)
-C6 = Checkbutton(window, text = "Romance", variable = v6)
-C7 = Checkbutton(window, text = "Fantasy", variable = v7)
-C8 = Checkbutton(window, text = "Psychological", variable = v8)
-C9 = Checkbutton(window, text = "School", variable = v9)
-C10 = Checkbutton(window, text = "Shounen", variable = v10)
-C11 = Checkbutton(window, text = "Magic", variable = v11)
-C12 = Checkbutton(window, text = "Slice of Life", variable = v12)
-C1.place(x=450, y=150)
-C2.place(x=520, y=150)
-C3.place(x=450, y=200)
-C4.place(x=520, y=200)
-C5.place(x=450, y=250)
-C6.place(x=520, y=250)
-C7.place(x=450, y=300)
-C8.place(x=520, y=300)
-C9.place(x=450, y=350)
-C10.place(x=520, y=350)
-C11.place(x=450, y=400)
-C12.place(x=520, y=400)
-
-####################  Dropdown lists
-Label(window, text = "To Do level : ", font = ("Times New Roman", 10)).place(x=450, y=25)
-n = StringVar()
-ToDoLvl = Combobox(window, width = 27, textvariable = n, state= "readonly")
-ToDoLvl['values'] = ('1', '2','3','4','5',)
-  
-ToDoLvl.place(x=450, y=50)
-ToDoLvl.current(1) 
-
-####################  Separators
-Vseparator = Separator(window, orient='vertical')
-Vseparator.place(relx=0.65, rely=0, relwidth=0, relheight=1)
-Vseparator2 = Separator(window, orient='vertical')
-Vseparator2.place(relx=0.45, rely=0, relwidth=0, relheight=1)
-Vseparator3 = Separator(window, orient='vertical')
-Vseparator3.place(relx=0.25, rely=0, relwidth=0, relheight=1)
-
-Hseparator = Separator(window, orient='horizontal')
-Hseparator.place(relx=0, rely=0.7, relwidth=1, relheight=1)
-
-
+app = Window(window)
 
 window.mainloop()
+
+####################  Labels
+# lbl=Label(window, text="Genres", fg='black', font=("Times New Roman", 12))
+# lbl.place(x=450, y=100)
+
+
+
+####################  Separators
+# Vseparator = Separator(window, orient='vertical')
+# Vseparator.place(relx=0.65, rely=0, relwidth=0, relheight=1)
+# Vseparator2 = Separator(window, orient='vertical')
+# Vseparator2.place(relx=0.45, rely=0, relwidth=0, relheight=1)
+# Vseparator3 = Separator(window, orient='vertical')
+# Vseparator3.place(relx=0.25, rely=0, relwidth=0, relheight=1)
+
+# Hseparator = Separator(window, orient='horizontal')
+# Hseparator.place(relx=0, rely=0.7, relwidth=0, relheight=0)
+
+
+
+
 
 
